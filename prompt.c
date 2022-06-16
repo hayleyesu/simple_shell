76fad5130248a2ba15139d7e850ea8659ba1c749
hayleyesu <101431878+hayleyesu@users.noreply.github.com>
2022-06-16 15:00:56 +0300

    Create prompt.c

---
+++ prompt.c
@@ -0,0 +1,12 @@
+#include "shell.h"
+/**
+* prompt - prompts the user to input with $
+* Return: void
+*/
+void prompt(void)
+{
+	if ((isatty(STDIN_FILENO) == 1) && (isatty(STDOUT_FILENO) == 1))
+		flags.interactive = 1;
+	if (flags.interactive)
+		write(STDERR_FILENO, "$ ", 2);
+}
